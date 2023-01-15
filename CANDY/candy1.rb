15ec7e1a7099449cb29afd6e2428054232f88921
david <62385136+wang-626@users.noreply.github.com>
2023-01-15 19:36:29 +0800

    Add type verify

--- CANDY/candy1.rb
+++ CANDY/candy1.rb
@@ -1,9 +1,14 @@
 class Candy1
   def initialize(list)
+    p list.class
+    if list.class != Array
+      raise ArgumentError, "錯誤的型別"
+    end
     @list = list
   end
 
   def test
    @list.min(2).sum
   end
+
 end
--- CANDY/candy1_spec.rb
+++ CANDY/candy1_spec.rb
@@ -1,6 +1,12 @@
 require "./candy1"
 RSpec.describe Candy1 do
-  describe "找出陣列最小的兩個值總和" do
+  describe "輸入值驗證" do
+    it "非陣列輸入" do
+      expect{ Candy1.new(123) }.to raise_error(ArgumentError,"錯誤的型別")
+    end
+  end
+
+  describe "功能測試：找出陣列最小的兩個值總和" do
     it "一般測試:[15,28,4,2,43]" do
       app = Candy1.new([15, 28, 4, 2, 43])
       expect(app.test()).to be 6
