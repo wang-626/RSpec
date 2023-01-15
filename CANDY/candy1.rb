42d0f758915632bd5021730b0e128c3dc6377fb8
david <62385136+wang-626@users.noreply.github.com>
2023-01-16 00:46:55 +0800

    Update candy1 test

--- CANDY/candy1.rb
+++ CANDY/candy1.rb
@@ -1,14 +1,26 @@
 class Candy1
   def initialize(list)
-    p list.class
-    if list.class != Array
-      raise ArgumentError, "錯誤的型別"
-    end
+    verify(list)
     @list = list
   end
 
-  def test
-   @list.min(2).sum
+  def calc
+    if @list.select { |i| i == @list.min }.length > 1
+      @list.min * 2
+    else
+      @list.min(2).sum
+    end
   end
 
+  private
+
+  def verify(list)
+    if list.class != Array
+      raise ArgumentError, "錯誤的型別"
+    elsif list.length < 2
+      raise ArgumentError, "陣列長度，不符合規定值"
+    elsif !list.all? { |v| v.is_a?(Integer) }
+      raise ArgumentError, "陣列中含有錯誤型別變數"
+    end
+  end
 end
--- CANDY/candy1_spec.rb
+++ CANDY/candy1_spec.rb
@@ -2,14 +2,27 @@ require "./candy1"
 RSpec.describe Candy1 do
   describe "輸入值驗證" do
     it "非陣列輸入" do
-      expect{ Candy1.new(123) }.to raise_error(ArgumentError,"錯誤的型別")
+      expect { Candy1.new(123) }.to raise_error(ArgumentError, "錯誤的型別")
+    end
+
+    it "陣列中長度小於2" do
+      expect { Candy1.new([]) }.to raise_error(ArgumentError, "陣列長度，不符合規定值")
+    end
+
+    it "陣列中有非數字變數" do
+      expect { Candy1.new([1, 2, 3, "aa"]) }.to raise_error(ArgumentError, "陣列中含有錯誤型別變數")
     end
   end
 
   describe "功能測試：找出陣列最小的兩個值總和" do
     it "一般測試:[15,28,4,2,43]" do
       app = Candy1.new([15, 28, 4, 2, 43])
-      expect(app.test()).to be 6
+      expect(app.calc()).to be 6
+    end
+
+    it "相同數字測試:[2,2,3,4]" do
+      app = Candy1.new([2,2,3,4])
+      expect(app.calc()).to be 4
     end
   end
 end
